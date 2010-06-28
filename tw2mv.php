5e4f65f6541252c4d62699b5d29bf4ed860e76ca
nojimage <nojimage at gmail.com>
2010-06-28 11:01:49 +0900

    version 2.1.3

--- tw2mv.php
+++ tw2mv.php
@@ -10,7 +10,7 @@
  * Redistributions of files must retain the above copyright notice.
  *
  * @filesource
- * @version   2.1.2
+ * @version   2.1.3
  * @author    nojimage <nojimage at gmail.com>
  * @copyright 2010 nojimage
  * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
